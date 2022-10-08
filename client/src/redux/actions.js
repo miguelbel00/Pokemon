a856b07435ff15ecff0a618e8073bea99c09ca50
unknown <miguel00@hotmail.com>
2022-10-08 15:47:33 -0500

    Actions and reducer added

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -2,10 +2,55 @@ export const GET_POKEMON = "GET_POKEMON";
 export const GET_ALL_POKEMONS = "GET_ALL_POKEMONS";
 export const CREATE_POKEMON = "CREATE_POKEMON";
 export const GET_ALL_TYPES = "GET_ALL_TYPES";
-export const GET_POKEMON_TYPES = "GET_ALL_TYPES";
+export const SET_FILTEREDS_POKEMONS = "GET_FILTEREDS_POKEMONS";
 
+const axios = require('axios')
 /* export const SET_PAGE = "SET_PAGE"; */
 
+
+
+export const getAllPokemons = dispatch => {
+
+        return axios
+          .get(`http://localhost:3001/pokemons`)
+          .then((response) =>
+            dispatch({ type: GET_ALL_POKEMONS, payload: response.data })
+          );
+      
+};
+export const getPokemon = idPokemon => dispatch => {
+
+        return axios
+          .get(`http://localhost:3001/pokemons/${idPokemon}`)
+          .then((response) =>
+            dispatch({ type: GET_ALL_POKEMONS, payload: response.data })
+          );
+      
+};
+export const createPokemon = pokemon => dispatch => {
+
+        return axios
+          .post(`http://localhost:3001/pokemons`,pokemon)
+          .then((response) =>
+            dispatch({ type: GET_ALL_POKEMONS, payload: response.data })
+          );
+      
+};
+export const getAllTypes = dispatch => {
+
+        return axios
+          .get(`http://localhost:3001/types`)
+          .then((response) =>
+            dispatch({ type: GET_ALL_POKEMONS, payload: response.data })
+          );
+      
+};
+export const setFilteredPokemons = pokemons => dispatch => {
+
+        return dispatch({ type: SET_FILTEREDS_POKEMONS, payload: pokemons })
+          
+      
+};
 
 
 
---
+++ client/src/redux/reducer.js
@@ -0,0 +1,91 @@
+import * as actions from "./actions";
+
+const initialState = {
+  types: [],
+  pokemons: [],
+  filteredPokemons: [],
+  pokemon: {},
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actions.CREATE_POKEMON:
+      return {
+        ...state,
+        pokemons: [...state.pokemons, action.payload],
+      };
+    case actions.GET_POKEMON:
+      return {
+        ...state,
+        pokemon: action.payload,
+      };
+    case actions.GET_ALL_POKEMONS:
+      return {
+        ...state,
+        houses: action.payload,
+      };
+    case actions.GET_ALL_TYPES:
+      return {
+        ...state,
+        types: action.payload,
+      };
+    case actions.SET_FILTEREDS_POKEMONS:
+      return {
+        ...state,
+        filteredPokemons: filterAndOrganize(state.pokemons,action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
+const filterAndOrganize = (pokemons, features) => {
+  return orderAttack(
+    orderName(
+      orderTypes(
+        originPokemons(pokemons, features.origin),
+        features.orderTypes
+      ),
+      features.orderName
+    ),
+    features.orderAttack
+  );
+};
+
+const originPokemons = (pokemons, feature) => {
+  feature === "Existing" &&
+    (pokemons = pokemons.filter((p) => !p.id.includes("-")));
+
+  feature === "Created" &&
+    (pokemons = pokemons.filter(
+      (p) => p.id.includes("-") && p.id.length === 36
+    ));
+
+  return pokemons;
+};
+
+const orderTypes = (pokemons, feature) => {
+  return feature === "Type"
+    ? pokemons
+    : pokemons.filter((p) => p.types.includes(feature));
+};
+const orderName = (pokemons, feature) => {
+  feature === "A-Z" && (pokemons = pokemons.sort());
+
+  feature === "Z-A" && (pokemons = pokemons.reverse());
+
+  return pokemons;
+};
+const orderAttack = (pokemons, feature) => {
+  feature === "Descending" &&
+    (pokemons = pokemons.sort((a, b) => {
+      return a - b;
+    }));
+  feature === "Ascending" &&
+    (pokemons = pokemons.sort((a, b) => {
+      return b - a;
+    }));
+  return pokemons;
+};
+
+export default rootReducer;
