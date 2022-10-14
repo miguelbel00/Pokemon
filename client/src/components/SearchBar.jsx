28fcaf2ad400bcdbd4204fac3695dff64c4d2c74
unknown <miguel00@hotmail.com>
2022-10-14 12:54:03 -0500

    pokemon search suggestion added, is lacking click on this

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { useDispatch} from "react-redux";
+import { useDispatch, useSelector} from "react-redux";
 import searchIcon from "../utils/search.png";
 import * as actions from "../redux/actions";
 import '../styles/SearchBar.css'
 
 const SearchBar = () => {
   const [word, setWord] = useState("");
+  const pokemonsState = useSelector((state) => state.pokemonsFiltered);
   const dispatch = useDispatch();
 
   const handleWord = (e) => {
@@ -25,6 +26,23 @@ const SearchBar = () => {
           value={word}
           onChange={handleWord}
         />
+        <div className="dropdown">
+        {pokemonsState.filter(p => {
+
+            let searchWord =  word.charAt(0).toUpperCase() + word.toLowerCase().slice(1);
+            return searchWord && p.name.startsWith(searchWord) && p.name !== searchWord
+        }).slice(0,5)
+        .map(p => (
+
+            <div key={p.id}onClick={handleWord} className="dropdown-row" value={p.name}>
+                <img src={p.image}  alt={p.name} /> 
+{            console.log(p.name)}
+                {p.name}
+            </div>
+
+        ))}
+
+        </div>
       </div>
       <div>
         <img
