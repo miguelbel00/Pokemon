51074739ad02993b1a36732b856a6a09a71f283d
unknown <miguel00@hotmail.com>
2022-10-14 21:08:59 -0500

    trying allos all urls from front

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-/* const cookieParser = require('cookie-parser'); */
+const cookieParser = require('cookie-parser'); 
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const pokemons = require('./routes/pokemons.js');
@@ -11,12 +11,12 @@ const server = express();
 
 server.name = 'API';
 
-/* server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' })); */
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
-/* server.use(cookieParser()); */
+server.use(cookieParser()); 
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pokemon.up.railway.app/'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://pokemon.up.railway.app/*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
